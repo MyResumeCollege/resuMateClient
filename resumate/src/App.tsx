232e495774f47d46056ac285d61343b3124cc082
amitbrickmanfa <amitbrickman@Amits-MacBook-Pro.local>
2024-05-18 12:42:16 +0300

    order + styling

--- resumate/src/App.tsx
+++ resumate/src/App.tsx
@@ -1,17 +1,21 @@
 import { useState } from "react";
 import "./App.css";
+import IntroForm from "./components/intro-form/IntroForm";
+import { GenerateResumeInput } from "./types/resume";
 import { generateResume } from "./services/openAIService";
+import Lottie from "react-lottie";
+import DocumentAnimation from './assets/animations/document.json';
 
 function App() {
-  const [name, setName] = useState<string>("");
-  const [job, setJob] = useState<string>("");
-  const [experience, setExperience] = useState<string>("");
-  const [description, setDescription] = useState<string>("");
-  const [resume, setResume] = useState<string>("");
+  const [resume, setResume] = useState<string>();
+  const [isGenerating, setIsGenerating] = useState(false);
 
-  const handleSubmit = async (e: React.FormEvent) => {
-    e.preventDefault();
+  const generatingResume = async ({ name,
+    job,
+    experience,
+    description }: GenerateResumeInput) => {
     try {
+      setIsGenerating(true);
       const generatedResume = await generateResume(
         name,
         job,
@@ -24,62 +28,32 @@ function App() {
       }, 2000);
     } catch (error) {
       console.error("Error generating resume:", error);
+    } finally {
+      setIsGenerating(false);
     }
   };
 
   return (
-    <form
-      onSubmit={handleSubmit}
-      style={{
-        display: "flex",
-        flexDirection: "column",
-        margin: "auto",
-        gap: "1rem",
-      }}
-    >
-      <label />
-      What is your name?
-      <input
-        type="text"
-        style={{
-          backgroundColor: "white",
-        }}
-        value={name}
-        onChange={(e) => setName(e.target.value)}
-      />
-      <label />
-      What job are you looking for?
-      <input
-        type="text"
-        style={{
-          backgroundColor: "white",
-        }}
-        value={job}
-        onChange={(e) => setJob(e.target.value)}
-      />
-      <label />
-      What is your experience?
-      <textarea
-        style={{
-          height: "5rem",
-          backgroundColor: "white",
-        }}
-        value={experience}
-        onChange={(e) => setExperience(e.target.value)}
-      />
-      <label />
-      Describe yourself in a few words.
-      <textarea
-        style={{
-          height: "10rem",
-          width: "20rem",
-          backgroundColor: "white",
-        }}
-        value={description}
-        onChange={(e) => setDescription(e.target.value)}
-      />
-      <button type="submit">Generate Resume</button>
-    </form>
+    <main className="app flex-1">
+      <IntroForm onSubmit={generatingResume} />
+      <div className="flex flex-col items-center justify-center px-[30px]">
+        {isGenerating && <div className="flex flex-col items-center">
+          <Lottie
+            options={{ animationData: DocumentAnimation, loop: true }}
+            style={{ height: 200, width: 200, filter: "drop-shadow(4px 4px 8px #bababa)" }}
+          />
+          <span style={{ color: "#707070" }}>Generating your resume...</span>
+        </div>}
+        {resume && !isGenerating &&
+          <span>
+            {resume}
+          </span>
+        }
+        {!resume && !isGenerating &&
+          <span>Your resume will be here</span>
+        }
+      </div>
+    </main>
   );
 }
 

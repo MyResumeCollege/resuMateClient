cf1651c92975b7b1a9fc84dde966e02671e3ca7b
Nofar Shapir <nofarshapir00@gmail.com>
2024-06-27 23:48:37 +0300

    covert text to pdf

--- resumate/src/router/router.tsx
+++ resumate/src/router/router.tsx
@@ -2,21 +2,21 @@ import { Dashboard } from "@/components/app/dashboard/Dashboard";
 import { BackgroundQuestionnaire } from "@/components/app/generating-proccess/background-questionnaire/BackgroundQuestionnaire";
 import { Generate } from "@/components/app/generating-proccess/generate/Generate";
 import { Start } from "@/components/app/generating-proccess/start/Start";
-import { ViewCV } from "@/components/app/generating-proccess/view-cv/ViewCV";
+import ViewCV from "@/components/app/generating-proccess/view-cv/ViewCV";
 import { createBrowserRouter } from "react-router-dom";
 
 export const router = createBrowserRouter([
-    {
-        path: "/",
-        element: <Dashboard />
-    },
-    {
-        path: "/build-cv",
-        children: [
-            { path: "", element: <Start /> },
-            { path: "background", element: <BackgroundQuestionnaire /> },
-            { path: "generate", element: <Generate /> },
-            { path: "view", element: <ViewCV /> },
-        ]
-    },
+  {
+    path: "/",
+    element: <Dashboard />,
+  },
+  {
+    path: "/build-cv",
+    children: [
+      { path: "", element: <Start /> },
+      { path: "background", element: <BackgroundQuestionnaire /> },
+      { path: "generate", element: <Generate /> },
+      { path: "view", element: <ViewCV /> },
+    ],
+  },
 ]);

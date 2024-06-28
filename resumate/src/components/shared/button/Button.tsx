bf359efa1eda1b3eb7b92498b138c5846bf9a0c3
amitbrickmanfa <amitbrickman@Amits-MacBook-Pro.local>
2024-06-28 08:26:49 +0300

    clean up and typos

--- resumate/src/components/shared/button/Button.tsx
+++ resumate/src/components/shared/button/Button.tsx
@@ -1,3 +1,5 @@
+import { CSSProperties } from "react";
+
 type ButtonVariant = 'primary' | 'secondary';
 
 type ButtonProps = {
@@ -8,9 +10,10 @@ type ButtonProps = {
     variant?: ButtonVariant;
     loading?: boolean;
     onClick?: () => void;
+    style?: CSSProperties;
 }
 
-export const Button = ({ text = 'Text is missing', disabled = false, loading = false, children, dense = false, variant = 'primary', onClick }: ButtonProps) => {
+export const Button = ({ text = 'Text is missing', disabled = false, loading = false, children, dense = false, variant = 'primary', onClick, style = {} }: ButtonProps) => {
     const classesByVariant: Record<ButtonVariant, string> = {
         primary: 'bg-primary',
         secondary: 'bg-accent'
@@ -22,7 +25,9 @@ export const Button = ({ text = 'Text is missing', disabled = false, loading = f
         disabled={disabled}
         type="button"
         onClick={onClick}
-        className={`transition-all w-full h-fit ${classesByDense} inline-flex justify-center items-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent text-white disabled:opacity-50 disabled:pointer-events-none hover:brightness-90 ${classesByVariant[variant]}`}>
+        className={`transition-all w-full h-fit ${classesByDense} inline-flex justify-center items-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent text-white disabled:opacity-50 disabled:pointer-events-none hover:brightness-90 ${classesByVariant[variant]}`}
+        style={style}
+    >
         {loading ?
             <span className="animate-spin inline-block size-5 border-[3px] border-current border-t-transparent text-white rounded-full" role="status" aria-label="loading"></span>
             : (children || text)}

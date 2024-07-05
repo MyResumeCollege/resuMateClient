7aeae949e9a5a3fd6296fc7d370fb3f2e7d2e75a
hadarza <hadar.zaguri@gmail.com>
2024-07-05 19:45:37 +0300

    google login

--- resumate/src/main.tsx
+++ resumate/src/main.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import { GoogleOAuthProvider } from "@react-oauth/google";
 import ReactDOM from "react-dom/client";
 import { Toaster } from "react-hot-toast";
 import { RouterProvider } from "react-router-dom";
@@ -6,13 +6,20 @@ import { RecoilRoot } from "recoil";
 import "./index.css";
 import { router } from "./router/router";
 
+const googleClientId = import.meta.env.VITE_REACT_APP_GOOGLE_CLIENT_ID;
+
+if (!googleClientId) {
+  throw new Error("Google Client ID is not defined in environment variables");
+}
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
+  <GoogleOAuthProvider clientId={googleClientId}>
     <RecoilRoot>
       <Toaster />
       <main className="resumate">
         <RouterProvider router={router} />
       </main>
     </RecoilRoot>
-  </React.StrictMode>
+  </GoogleOAuthProvider>
+
 );

6530725a3a781bab196291f072ca24cd5462071d
amitbrickmanfa <amitbrickman@Amits-MacBook-Pro.local>
2024-07-30 19:29:15 +0300

    link to dashboard from navbar

--- resumate/src/components/app/nav-bar/NavBar.tsx
+++ resumate/src/components/app/nav-bar/NavBar.tsx
@@ -1,12 +1,15 @@
+import { Link } from 'react-router-dom';
 import './navbar.css';
 
 export const NavBar = () => {
     return <nav className="h-[65px] bg-white p-5 border border-gray-300 flex justify-between items-center">
         <div className='flex items-center gap-2'>
             <div className='bg-primary h-[30px] w-[30px] rounded-lg'></div>
-            <span className="font-bold text-2xl logo relative">
-                ResuMate.
-            </span>
+            <Link to='/dashboard'>
+                <span className="font-bold text-2xl logo relative">
+                    ResuMate.
+                </span>
+            </Link>
         </div>
         <div className="">
             <img src='https://as2.ftcdn.net/v2/jpg/03/49/49/79/1000_F_349497933_Ly4im8BDmHLaLzgyKg2f2yZOvJjBtlw5.jpg'

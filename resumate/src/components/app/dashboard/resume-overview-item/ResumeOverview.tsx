3974b98785fd6a0fcf6a5f8990b54ebb32eb8d1b
Hadar Zaguri <48456834+hadarza@users.noreply.github.com>
2024-08-10 00:00:58 +0300

    show created date on preview CV (#80)

--- resumate/src/components/app/dashboard/resume-overview-item/ResumeOverview.tsx
+++ resumate/src/components/app/dashboard/resume-overview-item/ResumeOverview.tsx
@@ -8,7 +8,11 @@ type ResumeOverviewItemProps = {
 }
 
 export const ResumeOverviewItem = ({ resume, current, onSelect }: ResumeOverviewItemProps) => {
-
+    const formattedDate = new Date(resume.creationDate).toLocaleDateString("en-US", {
+        year: "numeric",
+        month: "long",
+        day: "numeric",
+    });
     return <div
         className={classNames(`bg-white p-5 border-[1px] rounded-md shadow-md cursor-pointer`,
             { '!bg-primary text-white': current },
@@ -17,7 +21,7 @@ export const ResumeOverviewItem = ({ resume, current, onSelect }: ResumeOverview
     >
         <div className="flex flex-col">
             <h3 className="font-bold text-md">{resume.jobTitle}</h3>
-            <h5 className="text-sm opacity-60">Generated at 27/07/2024</h5>
+            <h5 className="text-sm opacity-60">Generated on {formattedDate}</h5>
         </div>
     </div>
 

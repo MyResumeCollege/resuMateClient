3d056d7b058f258d57f0c88e8eda0cf001c30d9b
DorSonego <dorsonego123@gmail.com>
2024-08-11 19:17:36 +0300

    finish

--- resumate/src/components/app/login/Login.tsx
+++ resumate/src/components/app/login/Login.tsx
@@ -1,26 +1,26 @@
-import { Button } from "@/components/shared/button/Button";
-import { TextInput } from "@/components/shared/inputs/text-input/TextInput";
-import { useEffect, useState } from "react";
-import { Link, useNavigate } from "react-router-dom";
-import Resume from "@/assets/icons/resume.svg";
-import { useRecoilState } from "recoil";
-import { userState } from "../../../store/atoms/userAtom";
+import { Button } from '@/components/shared/button/Button';
+import { TextInput } from '@/components/shared/inputs/text-input/TextInput';
+import { useEffect, useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import Resume from '@/assets/icons/resume.svg';
+import { useRecoilState } from 'recoil';
+import { userState } from '../../../store/atoms/userAtom';
 import {
   saveTokens,
   googleSignIn,
   loginUser,
-} from "../../../services/authService";
-import { CredentialResponse, GoogleLogin } from "@react-oauth/google";
+} from '../../../services/authService';
+import { CredentialResponse, GoogleLogin } from '@react-oauth/google';
 
 export const Login = () => {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
   const [user, setUser] = useRecoilState(userState);
   const navigate = useNavigate();
 
   useEffect(() => {
     if (user) {
-      navigate("/build-cv");
+      navigate('/build-cv');
     }
   }, [user]);
 
@@ -36,14 +36,14 @@ export const Login = () => {
         refreshToken: loginGoogleRes.refreshToken,
       });
       setUser(loginGoogleRes.user);
-      navigate("/pricing");
+      navigate('/pricing');
     } catch (err) {
       console.log(err);
     }
   };
 
   const onGoogleLoginFailure = () => {
-    console.log("failed google log in");
+    console.log('failed google log in');
   };
 
   const handleLogin = async () => {
@@ -56,54 +56,54 @@ export const Login = () => {
         refreshToken: loginRes.refreshToken,
       });
       setUser(loginRes.user);
-      navigate("/dashboard");
+      navigate('/dashboard');
     } catch (err) {
       console.log(err);
     }
   };
 
   return (
-    <main className="flex-1 flex flex-col items-center justify-center">
-      <section className="flex gap-10 bg-white p-10 rounded-lg w-fit shadow-lg">
-        <div className="bg-primary rounded-md flex-1 px-8 flex items-center">
-          <img src={Resume} className="w-[250px]" />
+    <main className='flex-1 flex flex-col items-center justify-center'>
+      <section className='flex gap-10 bg-white p-10 rounded-lg w-fit shadow-lg'>
+        <div className='bg-primary rounded-md flex-1 px-8 flex items-center'>
+          <img src={Resume} className='w-[250px]' />
         </div>
-        <section className="flex flex-col gap-2 w-[400px]">
-          <h1 className="text-4xl font-bold mb-[20px]">Login</h1>
-          <TextInput value={email} onChange={setEmail} label="Email Address" />
+        <section className='flex flex-col gap-2 w-[400px]'>
+          <h1 className='text-4xl font-bold mb-[20px]'>Login</h1>
+          <TextInput value={email} onChange={setEmail} label='Email Address' />
           <TextInput
             value={password}
             onChange={setPassword}
-            type="password"
-            label="Password"
+            type='password'
+            label='Password'
           />
-          <div className="flex pt-2 text-sm items-center">
-            <div className="flex items-center gap-2">
-              <input type="checkbox" />
+          <div className='flex pt-2 text-sm items-center'>
+            <div className='flex items-center gap-2'>
+              <input type='checkbox' />
               <span>Remember me</span>
             </div>
-            <Link to={"/forgot-password"} className="ml-auto text-[red]">
+            <Link to={'/forgot-password'} className='ml-auto text-[green]'>
               Forgot Password?
             </Link>
           </div>
           <Button onClick={handleLogin}>Log In</Button>
-          <div className="text-center py-1">
-            <span className="text-sm">
-              Don't have an account?{" "}
-              <Link to="/register" className="text-primary font-medium">
+          <div className='text-center py-1'>
+            <span className='text-sm'>
+              Don't have an account?{' '}
+              <Link to='/register' className='text-primary font-medium'>
                 Sign Up
               </Link>
             </span>
           </div>
-          <div className="text-center py-6 flex items-center ">
-            <span className="h-[1px] bg-[black] flex-1 opacity-20"></span>
-            <span className="text-xs px-3 opacity-80">Or login with</span>
-            <span className="h-[1px] bg-[black] flex-1 opacity-20"></span>
+          <div className='text-center py-6 flex items-center '>
+            <span className='h-[1px] bg-[black] flex-1 opacity-20'></span>
+            <span className='text-xs px-3 opacity-80'>Or login with</span>
+            <span className='h-[1px] bg-[black] flex-1 opacity-20'></span>
           </div>
           <GoogleLogin
             width={400}
-            logo_alignment="center"
-            text="signin"
+            logo_alignment='center'
+            text='signin'
             onSuccess={onGoogleLoginSuccess}
             onError={onGoogleLoginFailure}
           />

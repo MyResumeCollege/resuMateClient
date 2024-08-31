d5e2e8b0c3bea523655789ee454b90118d9582d5
amitbrickmanfa <amitbrickman@Amits-MacBook-Pro.local>
2024-08-31 14:36:20 +0300

    Languages: new levels

--- resumate/src/components/resume-templates/Folder.tsx
+++ resumate/src/components/resume-templates/Folder.tsx
@@ -1,11 +1,9 @@
 import { TemplateProps } from "@/types/template-props";
+import { FaEnvelope, FaPhoneAlt } from "react-icons/fa";
 import { EditableText } from "../shared/editable-text/EditableText";
 import { RegenerateButton } from "../shared/regenerate-button/RegenerateButton";
-import { FaPhoneAlt, FaEnvelope } from "react-icons/fa";
-import { EducationPeriod } from "@/types/education-period";
-import { ExperiencePeriod } from "@/types/experience-period";
-import Experiences from "./shared/Experiences";
 import Educations from "./shared/Educations";
+import Experiences from "./shared/Experiences";
 
 const List = (listedText: string) => {
   return (listedText || "").split("\n").map((item) => (
--- resumate/src/types/language-knowledge.ts
+++ resumate/src/types/language-knowledge.ts
@@ -1,20 +1,28 @@
 export enum LanguageKnowledgeLevel {
-    Advanced,
+    Intermediate,
+    Proficient,
+    Fluent,
     Native,
 }
 
 export const LANGUAGE_LEVEL_NAME: Record<LanguageKnowledgeLevel, string> = {
-    [LanguageKnowledgeLevel.Advanced]: "Advanced",
+    [LanguageKnowledgeLevel.Intermediate]: "Intermediate",
+    [LanguageKnowledgeLevel.Proficient]: "Proficient",
+    [LanguageKnowledgeLevel.Fluent]: "Fluent",
     [LanguageKnowledgeLevel.Native]: "Native",
 }
 
 export const LANGUAGE_LEVEL_COLOR: Record<LanguageKnowledgeLevel, string> = {
-    [LanguageKnowledgeLevel.Advanced]: "rgb(234 179 8)",
+    [LanguageKnowledgeLevel.Intermediate]: "rgb(249 115 22)",
+    [LanguageKnowledgeLevel.Proficient]: "rgb(234 179 8)",
+    [LanguageKnowledgeLevel.Fluent]: "rgb(101 163 13)",
     [LanguageKnowledgeLevel.Native]: "rgb(21 128 61)",
 }
 
 export const LANGUAGE_LEVEL_COLOR_LIGHTEN: Record<number, string> = {
-    [LanguageKnowledgeLevel.Advanced]: 'rgb(253 230 138)',
+    [LanguageKnowledgeLevel.Intermediate]: 'rgb(254 215 170)',
+    [LanguageKnowledgeLevel.Proficient]: 'rgb(253 230 138)',
+    [LanguageKnowledgeLevel.Fluent]: 'rgb(217 249 157)',
     [LanguageKnowledgeLevel.Native]: 'rgb(187 247 208)'
 }
 
